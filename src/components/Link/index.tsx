e6ef8d55407518b964d08f1ff6a445be241883c9
Heojiyeon <33304871+Heojiyeon@users.noreply.github.com>
2023-08-21 15:23:32 +0900

    feat: linkData markup

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { links } from '../../utils/LinkData';
 
 export default function Link() {
-  return <div>Link</div>;
+  return (
+    <div>
+      <h1>Links</h1>
+      {links.map(link => (
+        <div key={link._id}>
+          <a href={link.link}>{link.title}</a>
+        </div>
+      ))}
+    </div>
+  );
 }
---
+++ src/utils/LinkData.ts
@@ -0,0 +1,14 @@
+const links = [
+  {
+    _id: 0,
+    title: 'Github',
+    link: 'https://github.com/Heojiyeon',
+  },
+  {
+    _id: 1,
+    title: 'Blog',
+    link: 'https://velog.io/@jiy7107',
+  },
+];
+
+export { links };
